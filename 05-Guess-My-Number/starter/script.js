312d3a3f7982f95ab3c207a8248964c444437495
Vito Vincenzo Covella <covinc93@gmail.com>
2022-04-20 23:11:52 +0200

    finished guess my number project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,27 @@
 'use strict';
+let secretNumber;
+let score;
+let highscore = 0;
+reset(20);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', mainGameLogic);
 
-document.querySelector('.check').addEventListener('click', function () {
+document.querySelector('.again').addEventListener('click', function () {
+  reset(20);
+  document.querySelector('.check').addEventListener('click', mainGameLogic);
+});
+
+function reset(scoreNum) {
+  secretNumber = Math.trunc(Math.random() * scoreNum) + 1;
+  score = scoreNum;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+}
+
+function mainGameLogic(event) {
   const guess = Number(document.querySelector('.guess').value);
 
   if (score <= 1) {
@@ -14,8 +31,15 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    event.currentTarget.removeEventListener('click', mainGameLogic);
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too high!';
     score--;
@@ -25,4 +49,4 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 
   document.querySelector('.score').textContent = score;
-});
+}
