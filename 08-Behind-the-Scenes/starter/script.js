4ecd6aba3cbabe12f9596c8e88b1a94b0fd838ab
Vito Vincenzo Covella <covinc93@gmail.com>
2022-04-22 12:43:40 +0200

    Finished scoping examples

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,7 +4,23 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
     console.log(output);
   }
 
@@ -14,4 +30,3 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
-console.log(age);
