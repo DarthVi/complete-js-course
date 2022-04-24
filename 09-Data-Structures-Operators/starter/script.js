cc73bc4171857448da562856b06803a262beebac
Vito Vincenzo Covella <covinc93@gmail.com>
2022-04-24 14:25:24 +0200

    added examples of optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -89,3 +89,17 @@ const restaurant = {
 
 // console.log(rest1);
 // console.log(rest2);
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.thu?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(users[0]?.name ?? 'users array empty');
