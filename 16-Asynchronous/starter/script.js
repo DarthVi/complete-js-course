7add0e9fd4782a6d5a18e39a5803c5cc8c77d783
Vito Vincenzo Covella <covinc93@gmail.com>
2022-05-18 21:18:11 +0200

    Wrapped fetch call and handled errors (starter directory of section 16)

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -58,8 +58,14 @@ const renderCountry = function (data, className = '') {
     </article>`;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  //   countriesContainer.style.opacity = 1;
 };
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //   countriesContainer.style.opacity = 1;
+};
+
 // const getCountryAndNeighbour = function (country) {
 //   // AJAX Call country 1
 //   const request = new XMLHttpRequest();
@@ -103,22 +109,39 @@ const renderCountry = function (data, className = '') {
 //     });
 // };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
   // Country 1
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
-      if (!neighbour) return;
+      if (!neighbour) throw new Error('No neighbour found');
       // Country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`); // we return another Promise
+      return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`); // we return another Promise
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbour'));
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again.`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
 
-getCountryData('italy');
+btn.addEventListener('click', function () {
+  getCountryData('italy');
+});
+
+// getCountryData('dsadasdsa');
+
 // getCountryAndNeighbour('italy');
 // getCountryData('italy');
 // getCountryData('usa');
