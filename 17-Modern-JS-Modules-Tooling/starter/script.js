89b975960760907c6edd3a134688e89e587c3da7
Vito Vincenzo Covella <covinc93@gmail.com>
2022-05-24 18:15:52 +0200

    experimenting with top-level await in modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,1 +1,11 @@
 import './clean.js';
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
